9f56f1a43494cc5c69d591d1a646574f22b94228
Stefan L <stefan.libiseller@gmail.com>
2015-05-17 05:36:27 +0200

    Update 2
    Nightly build, POI fenster gefixt-
    POI-Download OK? URL?

--- app/src/main/java/com/solution_driven/moc_maplocator/SelectPois.java
+++ app/src/main/java/com/solution_driven/moc_maplocator/SelectPois.java
@@ -1,38 +1,19 @@
 package com.solution_driven.moc_maplocator;
 
 import android.app.ListActivity;
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
-import android.widget.Toast;
 
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 
-/*
-*
-*/
 
 public class SelectPois extends ListActivity {
 
@@ -44,28 +25,28 @@ public class SelectPois extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
+        thisWindow = this;
+        setContentView(R.layout.activity_select_pois);
 
-        thisWindow=this;
-  /*
-     //   setContentView(R.layout.activity_select_pois);
 
-     //   adpt = new ArrayAdapter<String>(this, R.layout.list_pois,OptionListStrings);
-     //   setListAdapter(adpt);
+/*
+        adpt = new ArrayAdapter<String>(this, R.layout.activity_select_pois,OptionListStrings);
+        setListAdapter(adpt);
 
         ListView listView = getListView();
         listView.setTextFilterEnabled(true);
-
 */
 
-        (new DownloadPoisClient()).execute("http://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SEHENSWUERDIGOGD&srsName=EPSG:4326&outputFormat=json");
-
+        try {
+            (new DownloadPoisClient()).execute("http://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SEHENSWUERDIGOGD&srsName=EPSG:4326&outputFormat=json");
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
 
     }
 
-    public void sendMessage(View view)
-    {
+    public void sendMessage(View view) {
         Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
     }
